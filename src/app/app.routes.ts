344fb671188d3d14905587393e32ceb7feea0702
Onlyspirit <miquestdeji@gmail.com>
2025-10-20 15:30:51 +0100

    b

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Instructor } from './instructor/instructor';
+import { Erudite } from './erudite/erudite';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'instructor', component: Instructor },
+  { path: 'erudite', component: Erudite },
+  {path: '', redirectTo: 'instructor', pathMatch: 'full' }
+];
